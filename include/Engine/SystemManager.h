6b8e056e8f4adbc52a1589a07b0b7c47f57c7ae8
IsaiahKoamalu <ikbroderson@gmail.com>
2025-06-10 16:22:45 -1000

    Added SystemManager.h to register and retrieve systems.

---
+++ include/Engine/SystemManager.h
@@ -0,0 +1,39 @@
+#ifndef SYSTEMMANAGER_H
+#define SYSTEMMANAGER_H
+
+#include "System.h"
+#include <memory>
+#include <unordered_map>
+#include <typeindex>
+#include <cassert>
+
+/**
+ *@brief Responsible for registering and retrieving systems.
+ *       Every system operates on a filtered set of entities.
+ */
+class SystemManager {
+public:
+ template<typename T, typename... Args>
+ std::shared_ptr<T> registerSystem(Args&&... args) {
+  std::type_index typeID = typeid(T);
+  assert(systems.find(typeID) == systems.end() && "System ALREADY registered");
+
+  auto system = std::make_shared<T>(std::forward<Args>(args)...);
+  systems[typeID] = system;
+  return system;
+ }
+
+ template<typename T>
+ std::shared_ptr<T> getSystem() {
+  std::type_index typeID =typeid(T);
+  assert(systems.find(typeID) != systems.end() && "Ststem NOT registered");
+
+  return std::static_pointer_cast<T>(systems[typeID]);
+ }
+
+private:
+ std::unordered_map<std::type_index, std::shared_ptr<System>> systems;
+
+};
+
+#endif
